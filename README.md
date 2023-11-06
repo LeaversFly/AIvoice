2684bbfdf11a9afadcecad6940efa34b400de29c
Gin_99 <3348483879@qq.com>
2023-11-06 22:41:36 +0800

    factor: 重构

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # AIvoice
+
+<a>https://genshinvoice.top/v2/</a>的前端页面
+
+## 增添参数
+在src/config/params.js内添加参数，再在index.html内根据注释添加dom元素即可
+
+## 调试
+克隆项目后在根目录使用命令行"npm i"安装依赖，再使用"npm run dev"运行
--- src/config/params.js
+++ src/config/params.js
@@ -2,13 +2,13 @@ import roles from './roles.json'
 
 export const params = {
     text: '',
-    //最小值，最大值，步长，默认值
+    //拖动条：最小值，最大值，步长，默认值
     sdp: [0.0, 1.0, 0.1, 0.4],
-    // 人物太多使用文件导入
     speaker: roles,
     noise: [0.0, 1.5, 0.1, 0.6],
     noisew: [0.0, 1.5, 0.1, 0.8],
     length: [0.5, 2, 0.1, 1.0],
+    // 选择器：选项数组
     language: ['ZH', 'EN', 'JP', 'mix'],
     format: ['wav', 'mp3', 'ogg']
 }
